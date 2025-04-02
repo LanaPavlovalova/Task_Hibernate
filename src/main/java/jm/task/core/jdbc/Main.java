6e77483f272dca90daabdb857b6dd0e6aa3fdb6c
Lana Pavlova <lana_eva@list.ru>
2025-04-02 09:45:04 +0500

    Устранены замечания в классе Main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,45 +1,37 @@
 package jm.task.core.jdbc;
 
-import java.util.List;
-
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.service.UserService;
-import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-
+        UserDao userDao = new UserDaoHibernateImpl();
 
         // Создание таблицы
-        userDaoHibernate.createUsersTable();
+        userDao.createUsersTable();
 
         // Сохранение и вывод созданного юзера
-        userDaoHibernate.saveUser("Name1", "LastName1", (byte) 30);
-        userDaoHibernate.saveUser("Name2", "LastName2", (byte) 25);
-        userDaoHibernate.saveUser("Name3", "LastName3", (byte) 40);
-        userDaoHibernate.saveUser("Name4", "LastName4", (byte) 35);
+        userDao.saveUser("Name1", "LastName1", (byte) 30);
+        userDao.saveUser("Name2", "LastName2", (byte) 25);
+        userDao.saveUser("Name3", "LastName3", (byte) 40);
+        userDao.saveUser("Name4", "LastName4", (byte) 35);
 
         // Вывод всех юзеров
-        List<User> users = userDaoHibernate.getAllUsers();
+        List<User> users = userDao.getAllUsers();
         for (User user : users) {
             System.out.println(user);
         }
 
-        userDaoHibernate.removeUserById(1);
+        userDao.removeUserById(1);
 
         // Удаление всех юзеров
-        userDaoHibernate.cleanUsersTable();
+        userDao.cleanUsersTable();
 
         // Удаление таблицы совсем
-        userDaoHibernate.dropUsersTable();
+        userDao.dropUsersTable();
 
         Util.shutdown();
     }
